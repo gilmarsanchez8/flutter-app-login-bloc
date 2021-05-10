49641fa2fe5dbc97426028b3887256bae3cf35b8
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-05-10 18:54:38 -0500

    Diseno campos del login

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,6 +7,7 @@ class LoginPage extends StatelessWidget {
         body: Stack(
       children: [
         _crearFondo(context),
+        _loginForm(context),
       ],
     ));
   }
@@ -27,31 +28,113 @@ class LoginPage extends StatelessWidget {
       width: 100.0,
       height: 100.0,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(100.0),
-        color: Colors.black12
-      ),
+          borderRadius: BorderRadius.circular(100.0), color: Colors.black12),
     );
 
     return Stack(
       children: [
         fondoSuperiorLogin,
-        Positioned(child: circulo, top: 90.0,left: 30.0),
+        Positioned(child: circulo, top: 90.0, left: 30.0),
         Positioned(child: circulo, top: -40.0, right: -30.0),
         Positioned(child: circulo, bottom: -50.0, left: -10.0),
         Positioned(child: circulo, bottom: -10.0, right: -30.0),
-
         Container(
           padding: EdgeInsets.only(top: 80.0),
           child: Column(
             children: [
               Icon(Icons.person_pin_circle, color: Colors.white, size: 100.0),
-              SizedBox(height: 10.0, width: double.infinity,),
-              Text('Gilmar', style: TextStyle(color: Colors.white, fontSize: 25.0)),
-
+              SizedBox(
+                height: 10.0,
+                width: double.infinity,
+              ),
+              Text('Gilmar',
+                  style: TextStyle(color: Colors.white, fontSize: 25.0)),
             ],
           ),
         )
       ],
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SafeArea(
+              child: Container(
+            height: 200.0,
+          )),
+          Container(
+            width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 3.0,
+                    offset: Offset(0.0, 5.0),
+                    spreadRadius: 3.0,
+                  )
+                ]),
+            child: Column(
+              children: [
+                Text('Ingreso', style: TextStyle(fontSize: 20.0)),
+                SizedBox(height: 40.0),
+                _crearEmail(),
+                SizedBox(height: 20.0),
+                _crearPassword(),
+                SizedBox(height: 20.0),
+                _crearBoton(),
+              ],
+            ),
+          ),
+          Text('¿Olvidaste tu contraseña?'),
+          SizedBox(height: 100.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(Icons.alternate_email, color: Colors.black),
+            hintText: 'tuemail@email.com',
+            labelText: 'Email'),
+      ),
+    );
+  }
+
+  Widget _crearPassword() {
+    return Container(
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            icon: Icon(Icons.lock, color: Colors.black),
+            labelText: 'Contraseña'),
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      elevation: 0.0,
+      color: Colors.blue,
+      textColor: Colors.white,
+      onPressed: (){}
     );
   }
 }
