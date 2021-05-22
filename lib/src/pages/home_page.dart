ea5fee8f520c910dcdf47642827bbfa50bb64f2c
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-05-22 13:59:56 -0500

    Mostrar imagen en homepage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -44,11 +44,26 @@ class HomePage extends StatelessWidget {
       onDismissed: (direccion) {
         productosProvider.borrarProducto(producto.id);
       },
-      child: ListTile(
-        title: Text('${producto.titulo} - ${producto.valor}'),
-        subtitle: Text(producto.id),
-        onTap: () => Navigator.pushNamed(context, 'producto', arguments: producto)
-      ),
+      child: Card(
+        child: Column(
+          children: [
+            (producto.fotoUrl == null) 
+              ? Image(image: AssetImage('assets/no-image.png'))
+              : FadeInImage(
+                placeholder: AssetImage('assets/jar-loading.gif'), 
+                image: NetworkImage(producto.fotoUrl),
+                height: 300.0,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+                 ListTile(
+                  title: Text('${producto.titulo} - ${producto.valor}'),
+                  subtitle: Text(producto.id),
+                  onTap: () => Navigator.pushNamed(context, 'producto', arguments: producto)
+                ),
+          ],
+        ),
+      )
     );
   }
 
