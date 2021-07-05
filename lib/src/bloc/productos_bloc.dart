32e4bc9a441908e4d4c3f33e59a8adc90acfcae3
Gilmar Sanchez Cuartas <gsanchez@tsgroup.local>
2021-07-05 10:59:17 -0500

    Crear patron bloc CRUD productos

---
+++ lib/src/bloc/productos_bloc.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter_app_login_bloc/src/models/producto_model.dart';
+import 'package:flutter_app_login_bloc/src/providers/productos_provider.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Productobloc {
+  final _productosController = new BehaviorSubject<List<ProductoModel>>();
+  final _cargandoController = new BehaviorSubject<bool>();
+
+  final _productosProvider = new ProductosProvider();
+
+  Stream<List<ProductoModel>> get productosStream =>
+      _productosController.stream;
+
+  Stream<bool> get cargando => _cargandoController.stream;
+
+  void cargarProductos() async {
+    final productos = await _productosProvider.cargarProductos();
+    _productosController.sink.add(productos);
+  }
+
+  void agregarProducto(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.crearProducto(producto);
+    _cargandoController.sink.add(false);
+  }
+
+  Future<String> subirFoto(File foto) async {
+    _cargandoController.sink.add(true);
+    final fotourl = await _productosProvider.subirImagen(foto);
+    _cargandoController.sink.add(false);
+
+    return fotourl;
+  }
+
+  void editarProducto(ProductoModel producto) async {
+    _cargandoController.sink.add(true);
+    await _productosProvider.editarProducto(producto);
+    _cargandoController.sink.add(false);
+  }
+
+  void borrarProducto(String id) async {
+    await _productosProvider.borrarProducto(id);
+  }
+
+  dispose() {
+    _productosController?.close();
+    _cargandoController?.close();
+  }
+}
